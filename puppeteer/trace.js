f39e276bc9a1879a60c038788866620ab7ad00bf
xythree <xythree@163.com>
2018-01-11 15:33:48 +0800

    trace.js updated

--- puppeteer/trace.js
+++ puppeteer/trace.js
@@ -6,11 +6,12 @@ const iphone = devices["iPhone 6"]
  * 捕获网站的时间线跟踪，以进行诊断性能问题
  */
 
-const browser = await puppeteer.launch()
-const page = await browser.newPage()
+puppeteer.launch().then(async browser => {
+	const page = await browser.newPage()
 
-await page.emulate(iphone)
-await page.tracing.start({ path: "./trace.json" })
-await page.goto("https://www.xythree.com/")
-await page.tracing.stop()
-await browser.close()
+	await page.emulate(iphone)
+	await page.tracing.start({ path: "./trace.json" })
+	await page.goto("https://www.xythree.com/")
+	await page.tracing.stop()
+	await browser.close()
+})
